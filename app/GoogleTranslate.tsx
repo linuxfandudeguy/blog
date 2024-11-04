78d9d049e3433b23ff621a64b00c64ede1beb743
lelbois <164905463+linuxfandudeguy@users.noreply.github.com>
2024-11-03 20:19:14 -0500

    Update GoogleTranslate.tsx

--- app/GoogleTranslate.tsx
+++ app/GoogleTranslate.tsx
@@ -1,34 +1,34 @@
+// components/GoogleTranslate.tsx
 "use client";
 
-// components/GoogleTranslate.tsx
 import React, { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    google?: any;
+  }
+}
 
 const GoogleTranslate: React.FC = () => {
     useEffect(() => {
-        // Initialize Google Translate
         const googleTranslateElementInit = () => {
             new window.google.translate.TranslateElement(
                 {
-                    pageLanguage: 'en', // Default language of the page
-                    includedLanguages: '', // Leave blank to include all languages
+                    pageLanguage: 'en',
+                    includedLanguages: '',
                     layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE,
                 },
                 'google_translate_element'
             );
         };
 
-        // Load the Google Translate script
         const script = document.createElement('script');
         script.src = 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
         script.async = true;
         
-        // Attach the init function to the window object
         window.googleTranslateElementInit = googleTranslateElementInit;
-
-        // Add the script to the document
         document.body.appendChild(script);
 
-        // Clean up script and function on unmount
         return () => {
             document.body.removeChild(script);
             delete window.googleTranslateElementInit;
